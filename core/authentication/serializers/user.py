d35a3ab821e53976f5e75718eae18947b3448430
jonatasperaza <jonatassilvaperaza@gmail.com>
2025-03-04 21:46:00 -0300

    FEAT: Adicionar campo de foto de perfil ao serializer de usuário e implementar lógica de criação

--- core/authentication/serializers/user.py
+++ core/authentication/serializers/user.py
@@ -8,9 +8,24 @@ from core.uploader.utils.create_image import create_image
 User = get_user_model()
 
 class UserBasicSerializer(serializers.ModelSerializer):
+    profile_picture = serializers.ImageField(write_only=True, required=False)
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'name', 'photo_url')
+        fields = ('id', 'username', 'email', 'password', 'name', 'profile_picture', 'photo_url',)
+
+    def create(self, validated_data):
+        """Cria um usuário, faz o hash da senha e salva a foto de perfil."""
+        profile_picture = validated_data.pop('profile_picture', None)
+
+        user = User(**validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+
+        if profile_picture:
+            image = create_image(profile_picture, description="Foto de perfil", folder_path="media/profile")
+            user.photo_url = image.file 
+            user.save()
+        return user
 
 class UserSerializer(serializers.ModelSerializer):
     profile_picture = serializers.ImageField(write_only=True, required=False)
@@ -34,21 +49,6 @@ class UserSerializer(serializers.ModelSerializer):
             data.pop('user', None)  # remove a chave 'user' se existir
             return data
         return None
-
-    def create(self, validated_data):
-        """Cria um usuário, faz o hash da senha e salva a foto de perfil."""
-        profile_picture = validated_data.pop('profile_picture', None)
-
-        user = User(**validated_data)
-        user.set_password(validated_data['password'])
-        user.save()
-
-        if profile_picture:
-            image = create_image(profile_picture, description="Foto de perfil", folder_path="media/profile")
-            user.photo_url = image.file 
-            user.save()
-
-        return user
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
